3cfb81d21a9fe058dcb9e2fe7742932f9630920c
gcalcaterra <gustavocalcaterra@gmail.com>
2021-11-04 10:30:04 -0300

    Update ansible copy example

--- bash_prompt_tuning.sh
+++ bash_prompt_tuning.sh
@@ -1,6 +1,6 @@
 #Replace example.com with your own domain
 #Put this file in /etc/profile.d for system wide config. You can do this with ansible in this way for example:
-#ansible -i inventory/hosts all -m copy -a "src=bash_prompt_tuning.sh dest=/etc/profile.d/"
+#ansible -i inventory/hosts all -m copy -a "src=bash_prompt_tuning.sh dest=/etc/profile.d/ owner=root group=root mode=0644"
 
 if hostname -f | grep -q '\.prd\.example\.com\.py'; then
   _COLOR=31  # red
